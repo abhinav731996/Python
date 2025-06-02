e9fc7439c98cdb38b771e6da46e117ebb7be0d1a
Abhinav <abhinav731996@gmail.com>
2025-06-02 13:46:01 +0530

    practice python

---
+++ practice.py
@@ -0,0 +1,17 @@
+number1 = 10.13
+number = "abhinav"
+number2 = 12
+location = "rohtak"
+student = {"id":101, }
+
+
+print('Hello, python')
+print("Hello, python", number, number2)
+print(f"I am {number}, I live in {location}")
+print(f"hello {number}")
+print("*"*12)
+print("  Heading  ")
+print("*"*12)
+print(type(number))
+print(type(number2))
+print(type(number1))
