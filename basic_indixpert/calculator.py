f4e02b6c2fefb1d10959ecd404d591c2adb90fe4
Abhinav <abhinav731996@gmail.com>
2025-06-04 15:46:10 +0530

    python practice, userinput, calculator

---
+++ basic_indixpert/calculator.py
@@ -0,0 +1,19 @@
+print("*"*15)
+print("Calculator")
+print("*"*15)
+
+first  = input("Please enter first number: ")
+second = input("please enter second number: ")
+
+first = float(first)
+second = float(second)
+
+# sum = first+second
+print("*"*15)
+print("Result")
+print("*"*15)
+
+print("Sum:",first+second)
+print("Sub:",first-second)
+print("Mult:",first*second)
+print("Div:",first/second)
---
+++ basic_indixpert/practice.py
@@ -0,0 +1,17 @@
+data = {"breakfast": "8-9am", "lunch": "1=2pm", "dinner": "8-9pm"}
+data1 = {"breakfast1": "8-9am", "lunch1": "1=2pm", "dinner": "8-9pm"}
+
+data.pop("lunch")
+
+data.update(data1)
+
+name = {"Name": "Abhinav"}
+data1.update(name)
+
+# print(data.pop("breakfast"))
+
+# print(data)
+
+# print(data1)
+
+
