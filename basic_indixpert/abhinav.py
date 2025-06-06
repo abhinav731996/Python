29ab53cfc50a2debee49efcde29a98d6b7117237
Abhinav <abhinav731996@gmail.com>
2025-06-06 12:48:28 +0530

    day 5 (upper, lower, title case)

--- basic_indixpert/abhinav.py
+++ basic_indixpert/abhinav.py
@@ -2,8 +2,13 @@
 abhinav = {"breakfast":"8-9am", "lunch":"2-3pm", "dinner":"8-9pm"}
 abhinav["snacks"]="4-5pm"
 
-# del abhinav["lunch"]
+del abhinav["lunch"]
 
 abhinav.pop("dinner")
 
+abhinav.clear()
+
 print(abhinav)
+
+data = (10=="10")
+print(data)
---
+++ basic_indixpert/gulnaz.py
@@ -0,0 +1,11 @@
+Firstnumber= int(input(" enter the first number: "))
+Secondnumber= int(input(" enter the second number: "))
+
+Data={
+    "Addition":Firstnumber + Secondnumber ,
+    "substraction":Firstnumber - Secondnumber ,
+    "Multiplication":Firstnumber * Secondnumber ,
+    "Divison":Firstnumber / Secondnumber 
+}
+
+print(Data)
