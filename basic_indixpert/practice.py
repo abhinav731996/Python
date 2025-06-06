9c707bfc285da5d3340f9eb1bff1f5c289225bee
Abhinav <abhinav731996@gmail.com>
2025-06-06 17:38:55 +0530

    if else

--- basic_indixpert/practice.py
+++ basic_indixpert/practice.py
@@ -1,17 +1,19 @@
-data = {"breakfast": "8-9am", "lunch": "1=2pm", "dinner": "8-9pm"}
-data1 = {"breakfast1": "8-9am", "lunch1": "1=2pm", "dinner": "8-9pm"}
+# data = {"breakfast": "8-9am", "lunch": "1=2pm", "dinner": "8-9pm"}
+# data1 = {"breakfast1": "8-9am", "lunch1": "1=2pm", "dinner": "8-9pm"}
 
-data.pop("lunch")
+# data.pop("lunch")
 
-data.update(data1)
+# data.update(data1)
 
-name = {"Name": "Abhinav"}
-data1.update(name)
+# name = {"Name": "Abhinav"}
+# data1.update(name)
 
-# print(data.pop("breakfast"))
+# # print(data.pop("breakfast"))
 
-# print(data)
+# # print(data)
 
-# print(data1)
+# # print(data1)
 
 
+
+list = ["apple", "banana","mango" ]
