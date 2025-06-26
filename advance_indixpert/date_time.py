fcd9c93240df25a185e987333ab397d97bd6b31e
Abhinav <abhinav731996@gmail.com>
2025-06-26 12:04:33 +0530

    start advance

---
+++ advance_indixpert/date_time.py
@@ -0,0 +1,36 @@
+# import datetime
+
+# todaytime = datetime.datetime.now()
+
+# print(todaytime)
+
+
+
+# *********************************************
+# from datetime import datetime
+
+# data = datetime.now()
+
+# todaytime = data.strftime("%d-%m-%y__%H:%M")
+
+# print(todaytime)
+
+
+# *********************************************
+# import time
+
+# print("Searching........")
+
+# for n in range(1,5):
+#     time.sleep(2)
+#     print("working....")
+
+# print("Welcom Abhinav!!")
+
+
+# *********************************************
+
+from datetime import datetime
+
+data = datetime.now()
+
