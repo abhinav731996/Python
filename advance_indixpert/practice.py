cf7ed6f9f998e58155802a8d9481b411fd367202
Abhinav <abhinav731996@gmail.com>
2025-07-11 18:06:18 +0530

    classes day 2

--- advance_indixpert/practice.py
+++ advance_indixpert/practice.py
@@ -138,16 +138,3 @@ import datetime
 
 
 # ------------------------------------------------------------
-
-for i in range(1, n):
-    key = arr[i]
-    j = i - 1
-    
-     
-    while j >= 0 and arr[j] > key:
-        arr[j + 1] = arr[j]
-        j -= 1
-        
-    arr[j + 1] = key
-
-print("Sorted array:", arr)
