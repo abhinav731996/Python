9663589d38eb3e1e066b86fd89b2ddbf0bce3ef5
Abhinav <abhinav731996@gmail.com>
2025-07-15 18:46:15 +0530

    inheritance

---
+++ oops/inheritance/main.py
@@ -0,0 +1,36 @@
+
+# class Animal:
+
+#     def speak(self):
+#         print("animal can speak")
+
+#     def eat(self):
+#         print("animal can eat")
+
+# class Dog(Animal):
+#     def bark(speak):
+#         print("dog can bark")
+
+
+
+
+# class Father:
+#     def father(self):
+#         print("farming")
+
+# class Mother:
+#     def mother(self):
+#         print("cooking")
+
+# class child(Father,Mother):
+#     def coding(self):
+#         print("Coding")
+
+# ob = child()
+
+# ob.father()
+# ob.mother()
+# ob.coding()
+
+
+
